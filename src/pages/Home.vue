42627f6e76a3864c12d05eaaf453ea509a5935df
bad-commit <108960062+bad-commit@users.noreply.github.com>
2022-11-17 19:01:36 -0300

    Initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dropdown">
+    <button class="button" @click.stop="showMenu()">Toggle</button>
+    <div class="menu" :style="{ display: active ? 'block' : 'none' }">
+      <ul @click.stop>
+        <li>a</li>
+        <li>b</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: false,
+    };
+  },
+  methods: {
+    showMenu() {
+      this.active = !this.active;
+    },
+    close() {
+      this.active = false;
+    },
+  },
+  mounted() {
+    document.addEventListener("click", this.close);
+  },
+  unmounted() {
+    document.removeEventListener("click", this.close);
+  },
+};
+</script>
